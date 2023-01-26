b586179e08b866785bc8e57a537b49ab68d23229
Hariettemaina <hariettemaina@gmail.com>
2023-01-26 17:34:46 +0300

    a struct for inserting a new todo

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,20 @@
 use chrono::NaiveDate;
 use diesel::prelude::*;
-
-#[derive(Queryable)]
+use serde::{Serialize, Deserialize};
+use crate::schema::todo;
+#[derive(Queryable,Serialize,Deserialize)]
 pub struct Todo {
     pub id: i32,
+    pub title: String,
+    pub contents : String,
+    pub completed: bool,
+    pub date_created: NaiveDate,
+}
+
+
+#[derive(Insertable)]
+#[table_name="todo"]
+pub struct NewTodo {
     pub title: String,
     pub contents : String,
     pub completed: bool,
